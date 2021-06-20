45ebc0db2454109291f76aab25a12f694e8c024d
Nirmal J <nirmalnes312000@gmail.com>
2021-06-20 12:37:25 +0530

    Initial
    Initial

---
+++ Assets/Scripts/ZoomWithMouseWheel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomWithMouseWheel : MonoBehaviour
+{
+    public float ScrollSpeed = 10;
+    public Camera ZoomCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ZoomCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   		
+		ZoomCamera.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed;
+     
+    }
+}
